1c4c26f48a9fef6c352ad524c576f0a6d5fe8d93
Anthony Carrasco <anthony.carrasco@attlaris.pe>
2018-04-02 17:13:39 -0500

    Funciones de module y session

--- WebContent/WEB-INF/views/about.jsp
+++ WebContent/WEB-INF/views/about.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
     <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+    
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,8 +9,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>Hola soy Index</h1>
-<a href='<c:url value="/about"/>'>Ir about</a>
-<a href='<c:url value="/admin"/>'>Ir admin</a>
+<h1>soy el about</h1>
 </body>
 </html>
--- src/com/springWeb/controller/IndexController.java
+++ src/com/springWeb/controller/IndexController.java
@@ -4,17 +4,32 @@
 package com.springWeb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  * @author Anthony Carrasco
  *
  */
 @Controller
+@SessionAttributes("resultado")
 public class IndexController {
 
 	@RequestMapping("/")
-	public String showIndex() {
+	public String showIndex(Model model) {
+		model.addAttribute("resultado", "Vengo del Controller - index");
 		return "index";
+	}
+	
+	@RequestMapping("/about")
+	public String showAbout() {
+		return "about";
+	}
+	
+	@RequestMapping("/admin")
+	public String showAdmin(Model model) {
+		model.addAttribute("mensaje", "Mensaje de Model");
+		return "admin";
 	}
 }
