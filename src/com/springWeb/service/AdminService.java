912eed8155fdc720875f6868be0ed9525869510c
Anthony Carrasco <anthony.carrasco@attlaris.pe>
2018-04-06 16:50:35 -0500

    16:50 - avance 33 - capa de servicio

--- src/com/springWeb/service/AdminService.java
+++ src/com/springWeb/service/AdminService.java
@@ -1,5 +1,14 @@
 package com.springWeb.service;
 
-public class AdminService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.springWeb.dao.AdminDao;
+
+@Service
+public class AdminService {
+ // es un rupper
+	//instancia
+	@Autowired
+	private AdminDao adminDao;
 }
